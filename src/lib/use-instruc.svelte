95a9f67aff0586625a703cfa62102e4755d76fe6
OpenSourceEdu2023 <87601841+uh-huh-ok@users.noreply.github.com>
2024-02-14 21:38:42 +1100

    Uploaded Files

---
+++ src/lib/use-instruc.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<div class = "use-instruc">
+    <h5>1. Opening the main file</h5>
+    <p>The "Main file" is either the MM3&4.tns or SM3&4.tns document. Open this on your TiNspire CAS.</p>
+    <h5>2. Getting into the Library</h5>
+    <p>Click on the library button (The book icon below delete). This should take you into the library.</p>
+    <img src = "/images/use-instruc-1.png" alt = "step-2-img" class = "step-img">
+    <h5>3. Getting to the UDF menu</h5>
+    <p>Click '6' to access the UDF menu inside the library</p>
+    <img src = "/images/use-instruc-2.png" alt = "step-3-img" class = "step-img">
+    <p><b>Note:</b> You may need to refresh libraries in order to see the UDFs. This is: Menu, 1, 7, 1</p>
+    <h5>4. Getting to the UDFs </h5>
+    <p>Scroll down to the folder of choice and click onto it. It will then display the UDFs.</p>
+    <p>The following example shows how to get the Newton's method UDF (which is in the calculus folder)</p>
+    <img src = "/images/use-instruc-3.png" alt = "step-4-img" class = "step-img">
+    <p>Afterwards, simply scroll to/click the UDF of your choice.</p>
+    <img src = "/images/use-instruc-4.png" alt = "step-5-img" class = "step-img">
+</div>
+
+<style>
+    .use-instruc {
+        cursor: default;
+    }
+    .step-img {
+        width: 50%;
+    }
+</style>
